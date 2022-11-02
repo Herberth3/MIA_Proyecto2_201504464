32ef3f6aafb87e2b4f80737758967dd2acffead6
Herberth3 <ibiem3@gmail.com>
2022-11-02 01:21:18 -0600

    Login submit implemented

--- File_Manager_GO/client/src/app/components/login/login.component.ts
+++ File_Manager_GO/client/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioService } from 'src/app/services/servicio.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  idparticion = "";
+  usuario = "";
+  contrasena = "";
+  isLogin: any;
+  userName = "";
+  consola = "";
+
+  constructor(public service: ServicioService) { }
 
   ngOnInit(): void {
   }
 
+  ejecutar() {
+    const cmd = "login -usuario=" + this.usuario + " -password=" + this.contrasena + " -id=" + this.idparticion;
+    const idP = this.idparticion;
+    const usr = this.usuario;
+    const pwd = this.contrasena;
+
+    if (idP == "") {
+      alert("Ingrese un ID para la particion!");
+      return
+    } else if (usr == "") {
+      alert("Ingrese un usuario!");
+      return
+    } else if (pwd == "") {
+      alert("Ingrese una contraseña!")
+      return
+    }
+
+    if (cmd != "") {
+      this.service.postEntrada(cmd).subscribe(async (res: any) => {
+        this.consola = await res.Consola + "\n";
+        this.isLogin = await res.IsLogin;
+        this.userName = await res.LoginName;
+
+        if (this.isLogin != 0 && this.userName != "") {
+          alert("Inicio de sesion correcta!");
+          this.idparticion = "";
+          this.usuario = "";
+          this.contrasena = "";
+        } else {
+          alert(this.consola);
+        }
+      });
+
+    }
+  }
 }
